cb9e16f5356f6851556c409e867af527c738f87d
Madalin Popa <contact@madalinpopa.com>
2023-10-08 17:43:51 +0300

    Update database settings

--- habitstacker/settings/base.py
+++ habitstacker/settings/base.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "habitstacker.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "database/db.sqlite3",
     }
 }
 
