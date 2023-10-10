f5a9afe93eab41f686925c6d6f547af8f5ac8a6d
Madalin Popa <contact@madalinpopa.com>
2023-10-10 18:20:53 +0300

    Update allowed hosts

--- habitstacker/settings/prod.py
+++ habitstacker/settings/prod.py
@@ -1,1 +1,13 @@
 from .base import *
+
+ALLOWED_HOSTS = [
+    "habitstacker.coderustle.com",
+    "habitstacker.azurewebsites.net",
+]
+
+# SECURITY
+# -----------------------------------------------------------------------------
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+CSRF_TRUSTED_ORIGINS = ["https://habitstacker.azurewebsites.ro"]
