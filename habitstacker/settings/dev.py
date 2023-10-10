d1694b25caa9d77b1a7e24d73f49a3ad4666c121
Madalin Popa <contact@madalinpopa.com>
2023-10-10 18:20:30 +0300

    Update database settings

--- habitstacker/settings/dev.py
+++ habitstacker/settings/dev.py
@@ -1,3 +1,13 @@
 from .base import *  # noqa: F403
 
 ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "database/dev.sqlite3",
+    }
+}
