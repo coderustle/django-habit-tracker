70615c9eced93f5dbe8e150565fbba4f5e47ac7a
Madalin Popa <contact@madalinpopa.com>
2023-10-11 17:51:05 +0300

    Add custom user model

--- habitstacker/users/models.py
+++ habitstacker/users/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    A custom user model that extends Django's built-in AbstractUser model.
+    """
+
+    class Meta:
+        db_table = "auth_user"
+
+    def __str__(self) -> str:
+        return self.username
