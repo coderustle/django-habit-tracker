4630de7474a8735b9dabfa377cb3d738b92343c8
Madalin Popa <contact@madalinpopa.com>
2023-10-11 18:56:22 +0300

    Add custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   mode: "jit",
   content: ["./habitstacker/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: { center: true },
+      colors: {
+        primary: "#FCBE5B",
+        secondary: "#F5604A",
+        third: "#7EB67E",
+        dark: "#202020",
+      },
+      fontFamily: {
+        worker: ["Work Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
