390cd0a4e7341781bb091a435e28b39212aa9e4d
Madalin Popa <contact@madalinpopa.com>
2023-10-12 19:36:33 +0300

    Add htmx response utility

---
+++ habitstacker/core/utils.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+from django.http import HttpResponseRedirect
+
+
+class HtmxResponseRedirect(HttpResponseRedirect):
+    """
+    This is a utility class to add HX-Redirect to response header
+    Thus, will let the htmx to redirect to a specific location.
+    """
+
+    def __init__(self, redirect_to: str, *args: Any, **kwargs: Any) -> None:
+        super().__init__(redirect_to, *args, **kwargs)
+        self["HX-Redirect"] = self["Location"]
