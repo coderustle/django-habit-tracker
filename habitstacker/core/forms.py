65e2488a40fe35bebc538a896af304887ccc58b0
Madalin Popa <contact@madalinpopa.com>
2023-10-12 21:35:40 +0300

    Feat: add habit models and update navigation (#22)

---
+++ habitstacker/core/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Habit
+
+
+class AddHabitForm(forms.ModelForm):
+    """
+    A form used to add a new habit to the user's habit list.
+    """
+
+    class Meta:
+        model = Habit
+        fields = ["name"]
--- habitstacker/users/urls.py
+++ habitstacker/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import user_login, user_register
+from .views import user_login, user_logout, user_register
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", view=user_register, name="register"),
     path("login", view=user_login, name="login"),
+    path("logout/", view=user_logout, name="logout"),
 ]
