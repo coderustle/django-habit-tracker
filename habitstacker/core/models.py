21ae1708a72789781e58b9184025bfc55cb19430
Madalin Popa <contact@madalinpopa.com>
2023-10-12 20:40:23 +0300

    Add habit model

--- habitstacker/core/models.py
+++ habitstacker/core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Habit(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
