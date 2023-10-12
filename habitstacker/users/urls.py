fa737f8aa78785a0feb5169bfd4aa8cabc61acc4
Madalin Popa <contact@madalinpopa.com>
2023-10-12 20:02:53 +0300

    Feat: finish user login functionality (#20)

--- habitstacker/users/urls.py
+++ habitstacker/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import register
+from .views import user_login, user_register
 
 app_name = "users"
 
 urlpatterns = [
-    path("register/", view=register, name="register"),
+    path("register/", view=user_register, name="register"),
+    path("login", view=user_login, name="login"),
 ]
--- habitstacker/users/views.py
+++ habitstacker/users/views.py
@@ -1,16 +1,18 @@
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
-from django.shortcuts import redirect
 from django.template.response import TemplateResponse
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_http_methods
 
+from ..core.utils import HtmxResponseRedirect
 from .forms import RegisterUserForm
 
 
 @require_http_methods(["GET", "POST"])
-def register(request: HttpRequest) -> HttpResponse:
+def user_register(request: HttpRequest) -> HttpResponse:
     """
     View function that handles user registration.
 
@@ -35,9 +37,45 @@ def register(request: HttpRequest) -> HttpResponse:
             backend = "django.contrib.auth.backends.ModelBackend"
             login(request=request, user=user, backend=backend)
             messages.success(request, "Registration successful")
-            return redirect("core:home")
+            return HtmxResponseRedirect(redirect_to=reverse_lazy("core:home"))
         else:
             messages.error(request, str(form.errors))
-            return TemplateResponse(request, template, {"form": form})
+            return HtmxResponseRedirect(redirect_to=reverse_lazy("user:login"))
 
+    return HttpResponse(status_code=400)
+
+
+@require_http_methods(["GET", "POST"])
+def user_login(request: HttpRequest) -> HttpResponse:
+    """
+    View function that handles user registration.
+
+    If the request method is GET, it renders the registration form.
+    If the request method is POST, it validates the form data and creates a new
+    user if valid.
+    """
+    template = "registration/login.html"
+    if request.htmx:
+        template = "registration/partials/login.html"
+
+    if request.method == "GET":
+        form = AuthenticationForm()
+        return TemplateResponse(request, template, {"form": form})
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request=request, user=user)
+                return HtmxResponseRedirect(
+                    redirect_to=reverse_lazy("core:home")
+                )
+            else:
+                messages.error(request, "Invalid username or password")
+                return HtmxResponseRedirect(
+                    redirect_to=reverse_lazy("user:login")
+                )
     return HttpResponse(status_code=400)
