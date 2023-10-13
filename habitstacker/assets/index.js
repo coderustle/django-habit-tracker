5da46b781ff918e01b2fb799d386a3d0cd27dffa
Madalin Popa <contact@madalinpopa.com>
2023-10-13 10:16:07 +0000

    Update navigation bar

--- habitstacker/assets/index.js
+++ habitstacker/assets/index.js
@@ -10,4 +10,16 @@ import "./images/favicon.ico";
 import "./images/apple-touch-icon.png";
 import "./images/android-chrome-192x192.png";
 
-console.log("Hello World!");
+/**
+ * Alpine JS
+ */
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+/**
+ * Htmx setup
+ */
+window.htmx = require("htmx.org");
