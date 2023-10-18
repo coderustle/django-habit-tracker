65f00858e8cbc43ec394d8ca6df9869ec2d5a597
Madalin Popa <contact@madalinpopa.com>
2023-10-18 08:59:20 +0300

    Register navbar url

--- habitstacker/core/urls.py
+++ habitstacker/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, home, navbar
 
 app_name = "core"
 
 urlpatterns = [
     path("", view=index, name="index"),
-    path("home/", view=index, name="home"),
+    path("home/", view=home, name="home"),
+    path("navbar/", view=navbar, name="navbar"),
 ]
