d563d5a1b01fbd700d96504f20d7caf04434f62d
Madalin Popa <contact@madalinpopa.com>
2024-01-05 22:50:39 +0200

    Feature: add habits app and refactor home page (#37)

--- habitstacker/core/urls.py
+++ habitstacker/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import index, home, navbar
+from .views import index, navbar
 
 app_name = "core"
 
 urlpatterns = [
     path("", view=index, name="index"),
-    path("home/", view=home, name="home"),
     path("navbar/", view=navbar, name="navbar"),
 ]
