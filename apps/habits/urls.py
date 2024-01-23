6fc69fe40fbaaeb6611b2afa223275623de12e2a
Madalin Popa <contact@madalinpopa.com>
2024-01-23 18:52:15 +0200

    Update log habit url

--- apps/habits/urls.py
+++ apps/habits/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import home, add_habit, log_habit
+from .views import add_habit, home, log_habit
 
 app_name = "habits"
 
 urlpatterns = [
     path("home/", view=home, name="home"),
     path("add/", view=add_habit, name="add"),
-    path("log/<int:id>", view=log_habit, name="log")
+    path("log/<int:pk>", view=log_habit, name="log"),
 ]
