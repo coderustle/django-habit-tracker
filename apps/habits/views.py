f19929873b99b3866c03b96a7c154ec8845ddc9f
Madalin Popa <contact@madalinpopa.com>
2024-01-23 18:51:59 +0200

    Update log habit view

--- apps/habits/views.py
+++ apps/habits/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.urls import reverse
-from django_htmx.http import HttpResponseClientRedirect
 from django.utils import timezone
-from guardian.shortcuts import get_objects_for_user, assign_perm
 from django.views.decorators.http import require_http_methods
+from django_htmx.http import HttpResponseClientRedirect
+from guardian.shortcuts import assign_perm, get_objects_for_user
 
 from .forms import AddHabitForm
-from .models import HabitLog
+from .models import Habit, HabitLog
 
 
 @login_required
@@ -72,7 +73,9 @@ def list_habit(request: HttpRequest) -> HttpResponse:
 
 
 @login_required
-def log_habit(request: HttpRequest, id: int) -> HttpResponse:
+def log_habit(request: HttpRequest, pk: int) -> HttpResponse:
     if request.method == "POST":
-        habit = ""
-        return HttpResponseClientRedirect(reverse("habits:home"))
+        habit = get_object_or_404(Habit, pk=pk)
+        if habit and request.user.has_perm("habits.view_habit", habit):
+            HabitLog(habit=habit).save()
+            return HttpResponseClientRedirect(reverse("habits:home"))
